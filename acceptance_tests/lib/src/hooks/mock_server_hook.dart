ebc5433bff8161eb4fefda6a0d0e3cfa3e2c40cb
Artur Wojciechowski <me@iama.re>
2022-04-04 20:04:29 +0200

    fix: acceptance_tests should skip invalid feature files

--- acceptance_tests/lib/src/hooks/mock_server_hook.dart
+++ acceptance_tests/lib/src/hooks/mock_server_hook.dart
@@ -9,27 +9,25 @@ class MockServerHook extends Hook {
   @override
   Future<void> onAfterScenarioWorldCreated(
     covariant PubNubWorld world,
-    String scenario,
+    ScenarioRunnable scenario,
     Iterable<Tag> tags,
   ) async {
     try {
-      var tagParts = tags
-          .firstWhere((element) => element.name.startsWith('@contract='))
-          .name
-          .split('=');
+      var tagParts = tags.firstWhere((element) => element.name.startsWith('@contract=')).name.split('=');
 
       var contract = tagParts.skip(1).first;
 
       // await Future.delayed(Duration(seconds: 3));
 
-      var res = await http.get(Uri.parse(
-          'http://localhost:8090/init?__contract__script__=$contract'));
+      var res = await http.get(Uri.parse('http://localhost:8090/init?__contract__script__=$contract'));
 
       // check for 200 response and fail scenario if not 200
 
       if (res.statusCode != 200) {
         throw Exception('Mock server is not ready for initialization.');
       }
+
+      scenario.metadata['contract'] = 'initialized';
       // ignore: empty_catches
     } on StateError {}
   }
@@ -38,25 +36,34 @@ class MockServerHook extends Hook {
 
   @override
   Future<StepResult?> onAfterStep(
-      covariant PubNubWorld world, String step, StepResult stepResult) async {
+      covariant PubNubWorld world, ScenarioRunnable scenario, String step, StepResult stepResult) async {
     // print(
     //     'Hook step: $step, result: ${stepResult.result} ${stepResult.resultReason}');
 
+    if (scenario.metadata['contract'] != 'initialized') {
+      return stepResult;
+    }
+
     if (stepResult.result == StepExecutionResult.pass) {
-      // await Future.delayed(Duration(seconds: 1));
-      var res = await http.get(Uri.parse('http://localhost:8090/expect'));
-      var data = json.decode(res.body);
+      try {
+        // await Future.delayed(Duration(seconds: 1));
+        var res = await http.get(Uri.parse('http://localhost:8090/expect'));
+        var data = json.decode(res.body);
 
-      var failedExp = data['expectations']['failed'] as List;
-      // var succeededExp = data['expectations']['succeeded'] as List;
-      // var pendingExp = data['expectations']['pending'] as List;
+        var failedExp = data['expectations']['failed'] as List;
+        // var succeededExp = data['expectations']['succeeded'] as List;
+        // var pendingExp = data['expectations']['pending'] as List;
 
-      // world.logger.severe(
-      //     '[Hook] x ${failedExp.length}, . ${pendingExp.length}, v ${succeededExp.length}');
+        // world.logger.severe(
+        //     '[Hook] x ${failedExp.length}, . ${pendingExp.length}, v ${succeededExp.length}');
 
-      if (failedExp.isNotEmpty) {
-        return StepResult(stepResult.elapsedMilliseconds,
-            StepExecutionResult.fail, 'Failed expectations: $failedExp.');
+        if (failedExp.isNotEmpty) {
+          return StepResult(
+              stepResult.elapsedMilliseconds, StepExecutionResult.fail, 'Failed expectations: $failedExp.');
+        }
+      } catch (e) {
+        return StepResult(
+            stepResult.elapsedMilliseconds, StepExecutionResult.error, 'Unable to verify expectations: $e.');
       }
     }
 
