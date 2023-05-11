4554f3b7d4a4b41e3153f266de41041e1d7b7978
Richard Muragijimana <beastar457@gmail.com>
2023-05-11 22:10:17 +0200

    clean-up

--- pubnub/test/unit/net/utils.dart
+++ pubnub/test/unit/net/utils.dart
@@ -8,13 +8,16 @@ void main() {
       expect(isHeaderForbidden('Content-Length'), equals(true));
     });
 
-    test('isHeaderForbidden should return true for any Sec- or Proxy- header', () {
+    test('isHeaderForbidden should return true for any Sec- or Proxy- header',
+        () {
       expect(isHeaderForbidden('Sec-Test-1'), equals(true));
       expect(isHeaderForbidden('Sec-Random-2'), equals(true));
       expect(isHeaderForbidden('Proxy-Whatever'), equals(true));
     });
 
-    test('isHeaderForbidden should return false for any other header not specified', () {
+    test(
+        'isHeaderForbidden should return false for any other header not specified',
+        () {
       expect(isHeaderForbidden('X-Custom-Header'), equals(false));
       expect(isHeaderForbidden('Content-Type'), equals(false));
     });
